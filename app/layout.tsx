3f0038d670c964d16fcc28279388a469cb7fe9dc
MarceloRodrigue <91852079+MarceloRodrigue@users.noreply.github.com>
2026-01-20 20:17:54 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
-export const metadata = {
-  title: "Explique pra mim",
-  description: "Explique qualquer coisa de forma simples usando IA"
-};
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
