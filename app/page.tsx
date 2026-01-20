5010af02c31a6a1e60d491bf3a66c4b6dd89a621
MarceloRodrigue <91852079+MarceloRodrigue@users.noreply.github.com>
2026-01-20 18:30:55 -0300

    page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Explique pra mim</h1>
+      <p>Seu app está rodando corretamente 🚀</p>
+    </main>
+  );
+}
